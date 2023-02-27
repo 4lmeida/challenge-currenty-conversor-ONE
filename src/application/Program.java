dfb8c475a1e92fdf356c05e497bda46106a55ec1
Luis Almeida <lferreira646@gmail.com>
2023-02-27 13:35:09 -0300

    fix: currency symbol and refactor

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,45 +15,34 @@ public class Program {
 		int wantContinue = 2;
 		Object[] items = { "Conversor de Moeda", "Conversor de temperatura" };
 		Object selectedValue;
+		Currency crr  = new Currency();
 
 		do {
 			selectedValue = JOptionPane.showInputDialog(null, "Escolha uma Opção", "Menu",
 					JOptionPane.INFORMATION_MESSAGE, null, items, items[0]);
 
 			if (selectedValue == "Conversor de Moeda") {
-				try {
-					String strValue = JOptionPane.showInputDialog("Insira um valor:");
+				String strValue = JOptionPane.showInputDialog("Insira um valor:");
 
-					while (!strValue.matches("^(-?)(0|([1-9][0-9]*))(\\.[0-9]+)?$") || strValue.isBlank()
-							|| strValue == null) {
+				while (!strValue.matches("^(-?)(0|([1-9][0-9]*))(\\.[0-9]+)?$") || strValue.isBlank()
+						|| strValue == null) {
 
-						JOptionPane.showMessageDialog(null, "Informe apenas numeros");
-						strValue = JOptionPane.showInputDialog("Insira um valor:");
-					}
-					
-					double inputValue = Double.parseDouble(strValue);
-
-					Currency crr = new Currency();
-
-					Object[] option = { "De Reais a Dólares", "De Reais a Euros", "De Reias a Libras",
-							"De Reais a Yenes", "De Reais a Won Coreano", "De Dólares a Reais", "De Euros a Reis",
-							"De Libras a Reais", "De  Yenes a  Reais", "De Won  Coreano a Reias" };
-
-					Object currency = JOptionPane.showInputDialog(null,
-							"Escolha a  moeda para a qual " + "você deseja girar seu dinheiro", "Moedas",
-							JOptionPane.WARNING_MESSAGE, null, option, option[0]);
-
-					crr.currencyConverter(currency, inputValue);
-
-					JOptionPane.showMessageDialog(null, crr.toString());
-
-					wantContinue = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
-
-
-				} 
-				catch (NullPointerException e) {
-					System.out.println("Valor digitado inválido: " + e.getMessage());
+					JOptionPane.showMessageDialog(null, "Informe apenas numeros");
+					strValue = JOptionPane.showInputDialog("Insira um valor:");
 				}
+
+				double inputValue = Double.parseDouble(strValue);
+
+				Object currency = JOptionPane.showInputDialog(null,
+						"Escolha a  moeda para a qual " + "você deseja girar seu dinheiro", "Moedas",
+						JOptionPane.WARNING_MESSAGE, null, crr.getOption(), crr.getOption());
+
+				if (currency != null) {
+					crr.currencyConverter(currency, inputValue);
+					JOptionPane.showMessageDialog(null, crr.toString());
+				}
+
+				wantContinue = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
 
 			} 
 			else if (selectedValue == "Conversor de temperatura") {
--- src/entities/Currency.java
+++ src/entities/Currency.java
@@ -4,21 +4,25 @@ public class Currency {
 
 	private String symbol;
 	private Double value;
+	
+	private Object[] option = { "De Reais a Dólares", "De Reais a Euros", "De Reias a Libras",
+			"De Reais a Peso Chileno", "De Reais a Peso Argentino", "De Dólares a Reais", "De Euros a Reis",
+			"De Libras a Reais", "De Peso Chileno a Reais", "De Peso Argentino a Reias" };
 
 	public Currency() {
 	}
 
-	public Currency(String name, Double value) {
-		this.symbol = name;
+	public Currency(String symbol, Double value) {
+		this.symbol = symbol;
 		this.value = value;
 	}
 
-	public String getName() {
+	public String getSymbol() {
 		return symbol;
 	}
 
-	public void setName(String name) {
-		this.symbol = name;
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
 	}
 
 	public Double getValue() {
@@ -28,45 +32,54 @@ public class Currency {
 	public void setValue(Double value) {
 		this.value = value;
 	}
+	
+
+	public Object[] getOption() {
+		return option;
+	}
+
+	public void setOption(Object[] option) {
+		this.option = option;
+	}
 
 	public double currencyConverter(Object obj, double quantity) {
 
 
 		switch (obj.toString()) {
 		case "De Reais a Dólares":
-			value = quantity / 5.17;
+			value = quantity * 0.19;
 			symbol = "US$";
 			break;
 		case "De Reais a Euros":
-			value = quantity / 5.52;
+			value = quantity * 0.18;
 			symbol = "€";
 			break;
 		case "De Reias a Libras":
-			value = quantity / 6.24;
+			value = quantity * 0.16;
 			symbol = "£";
 			break;
-		case "De Reais a Yenes":
-			value = quantity / 0.038;
-			symbol = "¥";
+		case "De Reais a Peso Chileno":
+			value = quantity * 160.47;
+			symbol = "$";
 			break;
-		case "De Reais a Won Coreano":
-			value = quantity / 0.0040;
-			symbol = "₩";
+		case "De Reais a Peso Argentino":
+			value = quantity * 38.04;
+			symbol = "Arg$";
 			break;
 		case "De Dólares a Reais":
-			value = quantity * 5.17;
+			value = quantity * 5.18;
 			break;
 		case "De Euros a Reis":
-			value = quantity * 5.52;
+			value = quantity * 5.50;
 			break;
 		case "De Libras a Reais":
-			value = quantity * 6.24;
+			value = quantity * 6.25;
 			break;
-		case "De  Yenes a  Reais":
+		case "De Peso Chileno a Reais":
 			value = quantity * 0.038;
 			break;
-		case "De Won  Coreano a Reias":
-			value = quantity * 0.0040;
+		case "De Peso Argentino a Reias":
+			value = quantity * 0.063;
 			break;
 		}
 
